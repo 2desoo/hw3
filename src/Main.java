400c9ff9980cbaf63d4f04207c499b584e87e7e6
desoo <marcus60.2001@gmail.com>
2023-10-31 13:36:43 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание №1");
+        byte a = 4;
+        short b = 9;
+        int c = 14;
+        long d = 26L;
+        float e = 5.78f;
+        double f = 13.65;
+        System.out.println("Значение переменной а, с типом byte равно " + a);
+        System.out.println("Значение переменной b, с типом short равно " + b);
+        System.out.println("Значение переменной c, с типом int равно " + c);
+        System.out.println("Значение переменной d, с типом long равно " + d);
+        System.out.println("Значение переменной e, с типом float равно " + e);
+        System.out.println("Значение переменной f, с типом double равно " + f);
+
+
+        System.out.println("Задание №2");
+        byte q = 67;
+        short w = -159;
+        short r = 569;
+        int y = 27897;
+        long t = 987678965549L;
+        float u = 2.786f;
+        double i = 27.12;
+        System.out.println("Значение переменной q, с типом byte = " + q);
+        System.out.println("Значение переменной w, с типом short = " + w);
+        System.out.println("Значение переменной r, с типом short = " + r);
+        System.out.println("Значение переменной y, с типом int = " + y);
+        System.out.println("Значение переменной t, с типом long = " + t);
+        System.out.println("Значение переменной u, с типом float = " + u);
+        System.out.println("Значение переменной i, с типом double = " + i);
+
+
+        System.out.println("Задание №3");
+        byte LudmilaPavlovna = 23;
+        byte AnnaSergeevna = 27;
+        byte EkaterinaAndreevna = 30;
+        short threeClasses = 3;
+
+
+    }
+}
